3171404c6509b616c0c1ec9492b518754c75df00
jowens25 <jowens@novuspower.com>
2026-02-10 15:23:21 -0600

    signing?

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 dpkg-buildpackage -us -uc -b
 
-mv ../*.deb ../*.buildinfo ../*.changes dist
+mv ../*.dsc ../*.tar.* ../*.buildinfo ../*.changes dist/
+
+
+
+#debsign -k ABF8C9E8DF6D4AFD02BA58DCBA050865951ED7DD
